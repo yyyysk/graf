0f5b092487262361fce1fbbf99d32baabe13e814
sakamura yosuke <yosuke_sakamura@waku-2.com>
2020-04-20 18:57:01 +0900

    WIP: front

--- src/components/Graff.tsx
+++ src/components/Graff.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, MouseEvent} from 'react';
+import React, {useState, useEffect, useRef, MouseEvent} from 'react';
 import { Button, CssBaseline, Container, Typography, TextField } from '@material-ui/core';
 
 interface Position {
@@ -11,7 +11,11 @@ const scrollY = (): number => document.documentElement.scrollTop || document.bod
 
 
 const Graff = () => {
+  const [color, setColor] = useState('#000000');
   const canvasRef = useRef(null);
+
+  let drawing = false;
+  let oldPos: Position;
 
   // Canvasのcontextを取得
   const getContext = (): CanvasRenderingContext2D => {
@@ -20,25 +24,24 @@ const Graff = () => {
   };
 
   // タッチpositionの取得
-  const getPosX = (e: MouseEvent<HTMLElement>): Position => {
-    if (!canvasRef.current) return {x: 0, y:0};
-
-    console.log(canvasRef.current);
-    const cnvRect: ClientRect = canvasRef.current!.getBoundingClientRect();
-    const x = e.clientX - cnvRect.right + scrollX();
-    const y = e.clientY - cnvRect.top + scrollY();
+  const getPos = (e: MouseEvent<HTMLElement>): Position => {
+    const cnvRect: ClientRect = canvasRef.current.getBoundingClientRect();
+    const x = e.clientX - cnvRect.left + scrollX();
+    const y = e.clientY - cnvRect.top;
 
     return {x,y};
   };
 
   // Canvasのリスナ設定
   const settingCanvas = (ctx: CanvasRenderingContext2D): void => {
-    ctx.strokeStyle = "#000000";
+    ctx.strokeStyle = color;
     ctx.lineWidth = 5;
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
 
     const cnv = canvasRef.current;
+    if (!cnv) return;
+
     // PC
     cnv.addEventListener("mousedown", (e: MouseEvent<HTMLElement>) => {
       console.log("mousedown");
@@ -48,8 +51,9 @@ const Graff = () => {
     cnv.addEventListener("mouseup", () => drawing = false, false);
     cnv.addEventListener("mousemove", (e: MouseEvent<HTMLElement>) => {
       const pos = getPos(e);
-      console.log("mousemove : x=" + pos.x + ", y=" + pos.y + ", drawing=" + drawing);
+      //      console.log("mousemove : x=" + pos.x + ", y=" + pos.y + ", drawing=" + drawing);
       if (drawing) {
+        ctx.strokeStyle = color;
         ctx.beginPath();
         ctx.moveTo(oldPos.x, oldPos.y);
         ctx.lineTo(pos.x, pos.y);
@@ -67,7 +71,7 @@ const Graff = () => {
 
   useEffect(() => {
     const ctx: CanvasRenderingContext2D = getContext();
-    ctx.fillRect(0, 0, 100, 100);
+    settingCanvas(ctx);
     ctx.save();
   });
 
@@ -78,12 +82,14 @@ const Graff = () => {
         <Typography component="div" style={{ height: '100vh', backgroundColor: '#fff' }}>
           <Typography component="h2" align="left">Graffiti</Typography>
           <Typography component="div" align="center">
-            <canvas ref={canvasRef} height='500' style={{width: '100%', border: '1px solid #eee'}}/>
+            <canvas ref={canvasRef} height='500' width='300' style={{border: '1px solid #eee'}}/>
           </Typography>
           <Typography component="div" align="center">
             <Button variant="contained" color="primary">
               次の人に渡す 
             </Button>
+            <input type="color" id="head" name="head" value={color} onChange={(e) => setColor(e.target.value)} />
+           <label htmlFor="head">color</label>
           </Typography>
         </Typography>
       </Container>
